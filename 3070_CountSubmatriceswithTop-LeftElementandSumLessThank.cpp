60fe00d427558639776be42c0df6add939ddeda0
Mohd Sahil <ms9929997@gmail.com>
2024-03-03 22:50:48 +0530

    .

---
+++ 3070_CountSubmatriceswithTop-LeftElementandSumLessThank.cpp
@@ -0,0 +1,145 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifdef LOCAL
+string to_string(char s) { return "'" + string(1,s) + "'"; }
+string to_string(string s) { return '"' + s + '"'; }
+string to_string(const char *s) { return to_string((string)s); }
+string to_string(bool b) { return (b ? "1" : "0"); }
+template <typename A, typename B>
+string to_string(pair<A, B> p) { return "(" + to_string(p.first) + ", " + to_string(p.second) + ")"; }
+template <typename T>
+string to_string(const std::vector<T> &vec)
+{
+    bool first = true;
+    string result = "{";
+    for (const auto &elem : vec)
+    {
+        if (!first)
+            result += ", ";
+        first = false;
+        result += to_string(elem);
+    }
+    result += "}";
+    return result;
+}
+template <typename T, typename Container, typename Compare>
+string to_string(const std::priority_queue<T, Container, Compare> &pq)
+{
+    auto temp = pq;
+    bool first = true;
+    string result = "{";
+    while (!temp.empty())
+    {
+        if (!first)
+            result += ", ";
+        first = false;
+        result += to_string(temp.top());
+        temp.pop();
+    }
+    result += "}";
+    return result;
+}
+// for queue
+template <typename T, typename Container>
+string to_string(const std::queue<T, Container> &q)
+{
+    auto temp = q;
+    bool first = true;
+    string result = "{";
+    while (!temp.empty())
+    {
+        if (!first)
+            result += ", ";
+        first = false;
+        result += to_string(temp.front());
+        temp.pop();
+    }
+    result += "}";
+    return result;
+}
+// for stack
+template <typename T, typename Container>
+string to_string(const std::stack<T, Container> &s)
+{
+    auto temp = s;
+    bool first = true;
+    string result = "{";
+    while (!temp.empty())
+    {
+        if (!first)
+            result += ", ";
+        first = false;
+        result += to_string(temp.top());
+        temp.pop();
+    }
+    result += "}";
+    return result;
+}
+template <typename A>
+string to_string(A v)
+{
+  bool first = true;
+  string res = "{";
+  for (const auto &x : v)
+  {
+    if (!first) res += ", ";
+    first = false;
+    res += to_string(x);
+  }
+  res += "}";
+  return res;
+}
+void debug_out() { cerr << endl; }
+template <typename Head, typename... Tail>
+void debug_out(Head H, Tail... T)
+{
+  cerr << " " << to_string(H);
+  debug_out(T...);
+}
+#define debug(...) cerr << "[" << #__VA_ARGS__ << "]:", debug_out(__VA_ARGS__)
+#else
+#define debug(...) 42
+#endif
+
+typedef long long ll;
+// #define int ll
+#define endl '\n'
+
+const ll mod = 1000000007;
+
+class Solution {
+public:
+    int countSubmatrices(vector<vector<int>>& grid, int k) {
+        int result = 0;
+        for(int i=0;i<grid.size();i++)
+        {
+            for(int j=0;j<grid[0].size();j++)
+            {
+                if(i>=1)grid[i][j]+=grid[i-1][j];
+                if(j>=1)grid[i][j]+=grid[i][j-1];
+                if(i>=1 && j>=1)
+                grid[i][j]-=grid[i-1][j-1];
+                if(grid[i][j]<=k)
+                result++;
+            }
+        }
+
+        return result;
+    }
+};
+
+signed main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    // grid = [[7,6,3],[6,6,1]], k = 18
+    
+    vector<vector<int>> grid = {{7,6,3},{6,6,1}};
+    int k = 18;
+    Solution sol;
+    cout<<sol.countSubmatrices(grid,k)<<endl;
+
+    return 0;
+}
