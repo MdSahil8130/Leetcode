a7268ae7e8371fb9428461c08c54092e382156d9
Mohd Sahil <ms9929997@gmail.com>
2024-03-08 20:03:34 +0530

    ques 3005

--- 01_temp.cpp
+++ 01_temp.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 #ifdef LOCAL
-string to_string(char s) { return "'" + string(1,s) + "'"; }
+string to_string(char s) { return "'" + string(1, s) + "'"; }
 string to_string(string s) { return '"' + s + '"'; }
 string to_string(const char *s) { return to_string((string)s); }
 string to_string(bool b) { return (b ? "1" : "0"); }
@@ -79,23 +79,24 @@ string to_string(const std::stack<T, Container> &s)
 template <typename A>
 string to_string(A v)
 {
-  bool first = true;
-  string res = "{";
-  for (const auto &x : v)
-  {
-    if (!first) res += ", ";
-    first = false;
-    res += to_string(x);
-  }
-  res += "}";
-  return res;
+    bool first = true;
+    string res = "{";
+    for (const auto &x : v)
+    {
+        if (!first)
+            res += ", ";
+        first = false;
+        res += to_string(x);
+    }
+    res += "}";
+    return res;
 }
 void debug_out() { cerr << endl; }
 template <typename Head, typename... Tail>
 void debug_out(Head H, Tail... T)
 {
-  cerr << " " << to_string(H);
-  debug_out(T...);
+    cerr << " " << to_string(H);
+    debug_out(T...);
 }
 #define debug(...) cerr << "[" << #__VA_ARGS__ << "]:", debug_out(__VA_ARGS__)
 #else
@@ -108,12 +109,65 @@ typedef long long ll;
 
 const ll mod = 1000000007;
 
+class Solution
+{
+public:
+    int minimumOperationsToMakeEqual(int x, int y)
+    {
+        int min_op = 0;
+        if (x == y)
+            return 0;
+        if (x < y)
+            return y - x;
+
+        unordered_set<int> vis;
+        queue<int> q;
+        q.push(x);
+
+        while (!q.empty())
+        {
+            int sz = q.size();
+            while (sz--)
+            {
+                int a = q.front();
+                q.pop();
+
+                if (vis.find(a) != vis.end())
+                    continue;
+
+                vis.insert(a);
+
+                if (a == y)
+                    return min_op;
+
+                if (a % 11 == 0)
+                {
+                    q.push(a / 11);
+                }
+                if (a % 5 == 0)
+                {
+                    q.push(a / 5);
+                }
+
+                q.push(a + 1);
+                q.push(a - 1);
+            }
+            min_op++;
+        }
+
+        return -1;
+    }
+};
 
 signed main()
 {
     ios_base::sync_with_stdio(0);
     cin.tie(0);
     cout.tie(0);
+
+    Solution sol;
+    int x = 26, y = 1;
+    cout << sol.minimumOperationsToMakeEqual(x, y) << endl;
 
     return 0;
 }
