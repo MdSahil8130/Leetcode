7fdcb0b47ae8a3b30f9afc1545d17f0966a1f2ed
Mohd Sahil <ms9929997@gmail.com>
2024-03-21 22:31:04 +0530

    reverse a linked list

---
+++ 206_ReverseLinkedList.cpp
@@ -0,0 +1,159 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifdef LOCAL
+string to_string(char s) { return "'" + string(1,s) + "'"; }
+string to_string(string s) { return '"' + s + '"'; }
+string to_string(const char *s) { return to_string((string)s); }
+string to_string(bool b) { return (b ? "1" : "0"); }
+template <typename A, typename B>
+string to_string(pair<A, B> p) { return "(" + to_string(p.first) + ", " + to_string(p.second) + ")"; }
+template <typename T>
+string to_string(const std::vector<T> &vec)
+{
+    bool first = true;
+    string result = "{";
+    for (const auto &elem : vec)
+    {
+        if (!first)
+            result += ", ";
+        first = false;
+        result += to_string(elem);
+    }
+    result += "}";
+    return result;
+}
+template <typename T, typename Container, typename Compare>
+string to_string(const std::priority_queue<T, Container, Compare> &pq)
+{
+    auto temp = pq;
+    bool first = true;
+    string result = "{";
+    while (!temp.empty())
+    {
+        if (!first)
+            result += ", ";
+        first = false;
+        result += to_string(temp.top());
+        temp.pop();
+    }
+    result += "}";
+    return result;
+}
+// for queue
+template <typename T, typename Container>
+string to_string(const std::queue<T, Container> &q)
+{
+    auto temp = q;
+    bool first = true;
+    string result = "{";
+    while (!temp.empty())
+    {
+        if (!first)
+            result += ", ";
+        first = false;
+        result += to_string(temp.front());
+        temp.pop();
+    }
+    result += "}";
+    return result;
+}
+// for stack
+template <typename T, typename Container>
+string to_string(const std::stack<T, Container> &s)
+{
+    auto temp = s;
+    bool first = true;
+    string result = "{";
+    while (!temp.empty())
+    {
+        if (!first)
+            result += ", ";
+        first = false;
+        result += to_string(temp.top());
+        temp.pop();
+    }
+    result += "}";
+    return result;
+}
+template <typename A>
+string to_string(A v)
+{
+  bool first = true;
+  string res = "{";
+  for (const auto &x : v)
+  {
+    if (!first) res += ", ";
+    first = false;
+    res += to_string(x);
+  }
+  res += "}";
+  return res;
+}
+void debug_out() { cerr << endl; }
+template <typename Head, typename... Tail>
+void debug_out(Head H, Tail... T)
+{
+  cerr << " " << to_string(H);
+  debug_out(T...);
+}
+#define debug(...) cerr << "[" << #__VA_ARGS__ << "]:", debug_out(__VA_ARGS__)
+#else
+#define debug(...) 42
+#endif
+
+typedef long long ll;
+// #define int ll
+#define endl '\n'
+
+const ll mod = 1000000007;
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+ 
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        ListNode* pre = NULL;
+        ListNode* curr = head;
+
+        while(curr)
+        {
+            auto forward = curr->next;
+            curr->next = pre;
+            pre = curr;
+            curr = forward;
+        }
+
+        return pre;
+    }
+};
+
+
+signed main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    ListNode* head = new ListNode(1);
+    head->next = new ListNode(2);
+    head->next->next = new ListNode(3);
+    head->next->next->next = new ListNode(4);
+    head->next->next->next->next = new ListNode(5);
+
+    Solution sol;
+    auto res = sol.reverseList(head);
+    while(res)
+    {
+        cout << res->val << " ";
+        res = res->next;
+    }
+
+    return 0;
+}
