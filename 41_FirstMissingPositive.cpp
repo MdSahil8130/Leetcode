af5bfefa40923f0baf75a629017121b5f8a4c3dc
Mohd Sahil <ms9929997@gmail.com>
2024-03-27 02:29:51 +0530

    more optimized approach fro first missing number

--- 41_FirstMissingPositive.cpp
+++ 41_FirstMissingPositive.cpp
@@ -108,37 +108,61 @@ typedef long long ll;
 #define endl '\n'
 
 const ll mod = 1000000007;
+// cin.tie(nullptr)->ios_base::sync_with_stdio(false);
 
-class Solution
-{
+
+
+class Solution {
 public:
-    int firstMissingPositive(vector<int> &nums)
-    {
+    int firstMissingPositive(vector<int>& nums) {
+        cin.tie(nullptr)->ios_base::sync_with_stdio(false);
+
         int n = nums.size();
-        
-        for(int i=0;i<n;i++)
-        {
-            if(nums[i]<=0)
-            nums[i] = n+1;
+
+        for (int i = 0; i < n; i++) {
+            while (nums[i] <= n && nums[i] >= 1 && nums[nums[i] - 1] != nums[i])
+                swap(nums[i], nums[nums[i] - 1]);
         }
 
-        for(int i=0;i<n;i++)
-        {
-            if(abs(nums[i])<=n && nums[abs(nums[i])-1]>0)
-            {
-                nums[abs(nums[i])-1]*=-1;
-            }
+        for (int i = 0; i < n; i++) {
+            if (nums[i] != i + 1)
+                return i + 1;
         }
 
-        for(int i=0;i<n;i++)
-        {
-            if(nums[i]>0)
-            return i+1;
-        }
-
-        return n+1;
+        return n + 1;
     }
 };
+
+// class Solution
+// {
+// public:
+//     int firstMissingPositive(vector<int> &nums)
+//     {
+//         int n = nums.size();
+        
+//         for(int i=0;i<n;i++)
+//         {
+//             if(nums[i]<=0)
+//             nums[i] = n+1;
+//         }
+
+//         for(int i=0;i<n;i++)
+//         {
+//             if(abs(nums[i])<=n && nums[abs(nums[i])-1]>0)
+//             {
+//                 nums[abs(nums[i])-1]*=-1;
+//             }
+//         }
+
+//         for(int i=0;i<n;i++)
+//         {
+//             if(nums[i]>0)
+//             return i+1;
+//         }
+
+//         return n+1;
+//     }
+// };
 
 signed main()
 {
